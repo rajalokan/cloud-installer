8ff1cc70e1fc497fc920274a5c7ed7953ab57bbe
Alok Kumar <rajalokan@gmail.com>
2017-04-08 15:49:30 +0530

    better script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -31,20 +31,21 @@ info_block "Bootstrapping ${DEPLOYMENT_TYPE} --- Start" 2> /dev/null ||
 
 sudo chown -R ${USER}:${USER} /opt
 
-info_block "Bootstrapping ${DEPLOYMENT_TYPE} --- END"
-exit 0
 
-# is_package_installed git || install_package git
-#
-# if [[ ! -d /opt/cloud-installer ]]; then
-#     git clone https://github.com/rajalokan/cloud-installer.git ${PROJECT_DIR}
-# fi
-#
-# # Check if ansible is installed, if not bootstrap ansible
-# ansible-playbook --help > /dev/null ||
-#     eval $(printf "%q\n" "${PROJECT_DIR}/scripts/bootstrap_ansible.sh")
-#
-# # Run playbook
-# pushd "${PROJECT_DIR}/ansible"
-#     ansible-playbook playbooks/${DEPLOYMENT_TYPE}.yml
-# popd
+is_package_installed git || install_package git
+
+if [[ ! -d /opt/cloud-installer ]]; then
+    git clone https://github.com/rajalokan/cloud-installer.git ${PROJECT_DIR}
+fi
+
+# Check if ansible is installed, if not bootstrap ansible
+ansible-playbook --help > /dev/null 2>&1 ||
+    eval $(printf "%q\n" "${PROJECT_DIR}/scripts/bootstrap_ansible.sh")
+
+# Run playbook
+pushd "${PROJECT_DIR}/ansible"
+    ansible-playbook -e deployment_type=${DEPLOYMENT_TYPE} playbooks/prepare.yml
+    ansible-playbook playbooks/${DEPLOYMENT_TYPE}.yml
+popd
+
+info_block "Bootstrapping ${DEPLOYMENT_TYPE} --- END"
