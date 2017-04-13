05abd088a348fcc68cdbb64683064b3814d8ce66
Alok Kumar <rajalokan@gmail.com>
2017-04-13 12:27:25 +0530

    refactored boot, delete and provision

--- ci.sh
+++ ci.sh
@@ -10,6 +10,14 @@ DEPLOYMENT_INSTNACE=${2:-}
 
 VAULT_OPTIONS="--vault-password-file ~/.vault_password"
 
-pushd "ansible"
-    ansible-playbook -e instance_name=${DEPLOYMENT_INSTNACE} ${VAULT_OPTIONS} --tags=${DEPLOYMENT_ACTION} playbooks/boot/boot.yml
-popd
+if [[ "${DEPLOYMENT_ACTION}" == 'provision' ]]; then
+    # Will run playbook locally on remote host
+    pushd "ansible"
+        ansible-playbook playbooks/${DEPLOYMENT_INSTNACE}.yml
+    popd
+else
+    # Launches VM and then runs playbooks on remote hosts under tmux session `bootstrap`
+    pushd "ansible"
+        ansible-playbook -e instance_name=${DEPLOYMENT_INSTNACE} ${VAULT_OPTIONS} playbooks/openstack/${DEPLOYMENT_ACTION}.yml
+    popd
+fi
