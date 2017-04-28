c8382385bf6b8444e51c4e50283a4475dce1d253
Alok Kumar <rajalokan@gmail.com>
2017-04-29 00:58:36 +0530

    changes for setting up guts

--- ci.sh
+++ ci.sh
@@ -14,7 +14,11 @@ if [[ "${DEPLOYMENT_ACTION}" == 'provision' ]]; then
     # Will run playbook locally on remote host
     pushd "ansible"
         ansible-playbook -e deployment_type=${DEPLOYMENT_INSTNACE} playbooks/prepare.yml
-        ansible-playbook playbooks/instances/${DEPLOYMENT_INSTNACE}.yml
+        if [[ $# > 2 ]]; then
+            ansible-playbook --tags $3 playbooks/instances/${DEPLOYMENT_INSTNACE}.yml
+        else
+            ansible-playbook playbooks/instances/${DEPLOYMENT_INSTNACE}.yml
+        fi
     popd
 else
     # Launches VM and then runs playbooks on remote hosts under tmux session `bootstrap`
